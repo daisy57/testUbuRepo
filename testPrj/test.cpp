69140266b7fa80d1a96591d22eb7577c21f29280
Daisy <daisy_csu@163.com>
2012-12-06 09:51:25 +0800

    version 3.0

--- testPrj/test.cpp
+++ testPrj/test.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // add updated info here to commit
+// add updated info here to commit to version 3.0
 int main(int argc, const char *argv[])
 {
     cout << "Hello, git test!" << endl;
